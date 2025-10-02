8e2462cf1fabf3c3e4af42d7e19f1ef5a0e99b4b
joeshaker <youssefshaker296@gmail.com>
2025-10-02 18:24:45 +0300

    Video&ModuleInstructorDash

--- src/app/Features/Instructor/instructor.routes.ts
+++ src/app/Features/Instructor/instructor.routes.ts
@@ -1,3 +1,4 @@
+import { CourseModule } from './Components/CrsModules/course-module/course-module';
 import { Courses } from './Components/Courses/courses/courses';
 import { Routes } from '@angular/router';
 import { Instructorcontainer } from './InstructorContainer/instructorcontainer/instructorcontainer';
@@ -19,6 +20,41 @@ export const routes: Routes = [
       {
         path:'courses',
         loadComponent: () => import('./Components/Courses/courses/courses').then(m => m.Courses)
+      }
+    ]
+  },
+  {
+    path: 'instructor',
+    component: Instructorcontainer,
+    children: [
+      { 
+        path: '', 
+        redirectTo: 'dashboard', 
+        pathMatch: 'full' 
+      },
+      { 
+        path: 'dashboard', 
+        loadComponent: () => import('./Components/Dashboard/insdashboard/insdashboard').then(m => m.Insdashboard) 
+      },
+      {
+        path:'courses',
+        loadComponent: () => import('./Components/Courses/courses/courses').then(m => m.Courses)
+      },
+      {
+        path:'module',
+        loadComponent:()=>import('./Components/CrsModules/course-module/course-module').then(m=>m.CourseModule)
+      },
+      {
+        path:'AddModule',
+        loadComponent:()=>import('./Components/AddModule/addmodule/addmodule').then(m=>m.Addmodule)
+      },
+      {
+        path:'Videos',
+        loadComponent:()=>import('./Components/videos/videos/videos').then(m=>m.Videos)
+      },
+      {
+        path:'AddVideo',
+        loadComponent:()=>import('./Components/AddVideo/add-video/add-video').then(m=>m.AddVideo)
       }
     ]
   }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { SimpleLayout } from './Shared/layouts/simple-Layout/simple-layout/simple-layout';
+import { Instructorcontainer } from './Features/Instructor/InstructorContainer/instructorcontainer/instructorcontainer';
 
 export const routes: Routes = [
     {
     path: '',
      component: SimpleLayout,
         loadChildren: () => import('./Features/Instructor/instructor.routes').then(m => m.routes),
-        data: { roles: ['instructor'] }
+        // data: { roles: ['instructor'] }
     }
 ];
