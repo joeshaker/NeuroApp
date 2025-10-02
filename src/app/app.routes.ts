2cc0d4659038e9ce246eae8036d6210bd432ba80
3omda <3omda@iti>
2025-10-02 14:57:28 +0300

    auth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
-import { SimpleLayout } from './Shared/layouts/simple-Layout/simple-layout/simple-layout';
 
 export const routes: Routes = [
     {
-    path: '',
-     component: SimpleLayout,
-        loadChildren: () => import('./Features/Instructor/instructor.routes').then(m => m.routes),
-        data: { roles: ['instructor'] }
+        path: 'auth',
+        loadChildren: () => import('./Features/Auth/auth.routes').then(m => m.authRoutes)
+    },
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    },
+    {
+        path: '**',
+        redirectTo: 'auth/login'
     }
 ];
