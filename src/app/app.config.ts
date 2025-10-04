cac7cefb60430a5a78562f05c8058afa4973ff19
joeshaker <youssefshaker296@gmail.com>
2025-10-04 12:44:43 +0300

    ggg

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,6 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './Core/interceptors/interceptors/auth-interceptor';
 
 import { routes } from './app.routes';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
